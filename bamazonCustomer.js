41fa66e77af14d5809d7d6907a5a18ac7ec51dc7
aj518480 <allisoncordovez@gmail.com>
2019-10-04 09:42:02 -0400

    Worked on making the promptCustomerForItem Function, I used the inquirer package to prompt the user to pick an item using the ID. Then proceeded to call the next function which would be promptCusomerforQuantity which also has a prompt from the Inquirer package and holds the user's answer with the promise .then

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
-//packages I will be using for 
+//packages I will be using for the Customer for Bamazon
 
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+
+
 
 var connection = mysql.createConnection({
   
@@ -14,51 +16,93 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: "root",
   database: "bamazonDB"
-});
-//create the conecction with the server and load the product data
-connection.connect(function(err) {
- 
-     if(err) {
-         throw err;
-     console.log("error coonect" + err.stack);
-     
-     }
 
- loadProducts()
+});
+
+
+//This function is to make sure we are connecting to our DB
+//Or to throw an error if we can not connect
+
+connection.connect(function(err) {
+   if (err){
+   console.log("error connect" + err.stack);
+   }
+// We are calling the next function
+
+   loadProducts();
+
     })
 
-//function to load products from db
+//Function to load products from db
+
 function loadProducts(){
- connection.query("SELECT * FROM products", function(err,res){
+connection.query("SELECT * FROM products", function(err,res){
+   if (err) throw err;   
+   console.table(res);
 
-    if(err)throw err
-
-    console.table(res);
-
-    promptCustomerforItem(res);
+   promptCustomerforItem(res);
  })
 }
  //prompt customer for a product ID
-   function promptCustomerforItem(){
+   function promptCustomerforItem(dbResponse){
+ 
+      inquirer
+  .prompt([
+     {
+    type:"input",
+    name:"chooseID",
+    message:"Please pick a item ID number"
+     }
+  ])
+  .then(answers => {
+    var customerChosenProduct= answers.chooseID;
+
+    promptCustomerForQuantity(customerChosenProduct,dbResponse);
+  });
 
    }
    //prompt customer for quantity
-function promptCustomerForQuantity(){
-
+function promptCustomerForQuantity(customerChosenProduct,dbResponse){
+   inquirer
+   .prompt([
+      {
+     type:"input",
+     name:"checkQuantity",
+     message:"How many would you like?"
+      }
+   ])
+   .then(answers => {
+     var howManyTheyChose= answers.checkQuantity;
+     console.log(howManyTheyChose,dbResponse.stock_quantity)
+     if(howManyTheyChose > dbResponse.stock_quantity){
+        console.log("Not enough in stock to purchase")
+     }
+     else{
+      makePurchase();
+      
+     }
+   });
 }
+
    //purchase function to a buy desired item
  function makePurchase(){
+    console.log("made purchase")
 
- }
+ };
 
- //check inventory to see if the user choice exist in
- function checkInventory(){
+//  //check inventory to see if the user choice exist in
+  function checkInventory(dbResponse){
 
- }
+//  }
 
- //check to see if user wants to quit the program(optional)
- function checkItShouldExit(){
+//  //check to see if user wants to quit the program(optional)
+//  function checksIfUserWantsToExit(){
 
- }
+//     //will need to ask user by inquirer
+
+// connection.end();//to exit sql
+// process.exit(0);// to exit node
+//  }
+ 
